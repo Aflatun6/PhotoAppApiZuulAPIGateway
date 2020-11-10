a5961945abe062a9fd124df55688ed7cf1692810
004462 <a.velibeyli6@gmail.com>
2020-11-10 16:56:18 +0400

    adding normal jwts conf

---
+++ src/main/java/com/avelibeyli/photoapp/api/gateway/security/AuthorizationFilter.java
@@ -0,0 +1,62 @@
+package com.avelibeyli.photoapp.api.gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+    private final Environment env;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager, Environment env, SecretKey secretKey, JwtConfig jwtConfig) {
+        super(authenticationManager);
+        this.env = env;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String authorizationHeader = request.getHeader(env.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null || !(authorizationHeader.startsWith(env.getProperty("authorization.token.header.prefix")))) {
+            chain.doFilter(request, response);
+            return;
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken); // this is like authmanager.authenticate(our Authentication)
+        chain.doFilter(request, response);
+
+
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(env.getProperty("authorization.token.header.name"));
+
+        String token = authorizationHeader.replace(env.getProperty("authorization.token.header.prefix"), "");
+        String userId = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) return null;
+
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>()); // this is the second authentication. Like first time we do it when we create user and login , next time we need to create again UsernamePasswordToken and authenticate it
+
+
+    }
+}
---
+++ src/main/java/com/avelibeyli/photoapp/api/gateway/security/WebSecurity.java
@@ -0,0 +1,44 @@
+package com.avelibeyli.photoapp.api.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.crypto.SecretKey;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+    private final Environment env;
+
+    @Autowired
+    public WebSecurity(SecretKey secretKey, JwtConfig jwtConfig, Environment env) {
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
+        this.env = env;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // this makes our spring security not to create session every time some client is signing in , so we need out request to be reauthorized every time they do request. OncePerRequest
+
+        http.authorizeRequests()
+                .antMatchers(env.getProperty("api.h2console.url")).permitAll()
+                .antMatchers(HttpMethod.POST, env.getProperty("api.signUp.url")).permitAll()
+                .antMatchers(HttpMethod.POST, env.getProperty("api.login.url")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthorizationFilter(authenticationManager(), env, secretKey, jwtConfig));
+    }
+}
